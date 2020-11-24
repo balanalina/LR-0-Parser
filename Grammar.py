f48760691ea807acba5f4403c8d88029eb041f9d
Balan Alina <alinabalan16.ab@gmail.com>
2020-11-24 14:32:04 +0200

    initial commit

---
+++ Grammar.py
@@ -0,0 +1,10 @@
+class Grammar:
+    def __init__(self):
+        self.non_terminal = []
+        self.terminal = []
+        self.production = []
+        self.read_grammar()
+
+    def read_grammar(self):
+        pass
+
