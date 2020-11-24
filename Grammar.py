d16af1e86be45c733a391fca6bc38443acebed36
Balan Alina <alinabalan16.ab@gmail.com>
2020-11-24 15:39:15 +0200

    lab work

--- Grammar.py
+++ Grammar.py
@@ -1,10 +1,38 @@
+import os
+import sys
+
+
 class Grammar:
-    def __init__(self):
+    def __init__(self, file_name):
         self.non_terminal = []
         self.terminal = []
-        self.production = []
+        self.production = {}
+        self.file = file_name
         self.read_grammar()
 
     def read_grammar(self):
-        pass
+        file = open(os.path.join(sys.path[0], self.file), 'r')
+        file_content = []
+        for line in file:
+            # we don't need the new line or any spaces
+            file_content.append(line.replace("\n", ""))
+        self.non_terminal = file_content[0].split(',')
+        self.terminal = file_content[1].split(',')
+        for i in range(2, len(file_content)):
+            symbol = file_content[i].split('->')[0]
+            productions = file_content[i].split('->')[1]
+            productions = productions.split("|")
+            self.production[symbol] = productions
 
+
+    def get_terminals(self):
+        return self.terminal
+
+    def get_non_terminals(self):
+        return self.non_terminal
+
+    def get_production(self):
+        return self.production
+
+
+# g = Grammar("Data/g1.in")
---
+++ Parser.py
@@ -0,0 +1,54 @@
+from Grammar import Grammar
+
+
+class Parser:
+    def __init__(self, grammar):
+        self.grammar = grammar
+        self.states = []
+
+    def goto(self, production):
+        pass
+
+    def closure(self, production):
+        pass
+
+    def canonnicalCollection(self):
+        pass
+
+
+    def menu(self):
+        s = "0. Exit \n"
+        s += "1. Set of non_terminals \n"
+        s += "2. Set of terminals \n"
+        s += "3. Set of productions \n"
+        s += "4. Production for a given non_terminal \n"
+        s += "5. Parse \n"
+        return s
+
+    def run(self):
+        while True:
+            print(self.menu())
+            print("Enter command: ")
+            command = int(input())
+            if command == 1:
+                print(self.grammar.get_non_terminals() + "\n")
+            elif command == 2:
+                print(self.grammar.get_terminals() + "\n")
+            elif command == 3:
+                for key in self.grammar.production.keys():
+                    prod = str(key) + " -> "
+                    for value in self.grammar.production[key]:
+                        prod += value + " | "
+                    print(prod)
+                print("\n")
+            elif command == 4:
+                pass
+            elif command == 5:
+                pass
+            else:
+                break
+
+
+gr = Grammar("Data/g1.in")
+g = Parser(gr)
+g.run()
